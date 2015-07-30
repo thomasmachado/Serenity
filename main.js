5367fe96bd7cd7fe8b44b36e450c05e684dbe193
thomasmachado <thomas.machado360@gmail.com>
2015-07-30 16:07:20 -0400

    Remove components and add story animation
    Removed components that were not being used to save space.
    
    fix #4: Added a story to the about section and made animation so that people can read their story without leaving the page.

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+    
+    $('#storyButton').click(function () {
+        $('#story').fadeIn('fast');
+    });
+    
+    $('#backButton').click(function () {
+        $('#story').fadeOut('fast');
+    });
+    
+    
+});
